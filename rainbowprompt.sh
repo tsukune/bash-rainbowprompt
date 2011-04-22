fe589272c224a09a6fb832c93a0fbe0a9167e8f8
tsukune <tshikanai+tsukune@gmail.com>
2011-04-22 20:15:56 +0900

    modified: substitute \h of $PS1 with raibowized \h

--- rainbowprompt.sh
+++ rainbowprompt.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
 
-# 虹色にする文字列とその文字数
-declare myhostname
-myhostname=`hostname -s`
-declare -i mojisu=${#myhostname}
+# Print HOSTNAME to a rainbow colors.
 
-# 虹色化
-declare -a color=(red yellow green cyan blue magenda)
-declare -i i=0
-declare rainbowstr=
+if echo ${PS1} | grep "\\\\h" ; then
+   declare sourcestr
+   sourcestr=`hostname -s`
+   declare -i mojisu=${#sourcestr}
 
-while [ ${mojisu} -gt 0 ]; do
+   # Rainbowize
+   declare -a color=(red yellow green cyan blue magenda)
+   declare -i i=0
+   declare rainbowstr=
 
-   # 文字を1文字切り出す
-   declare -i order=$i+1
-   letter=`echo ${myhostname} | cut -c${order}`
+   while [ ${mojisu} -gt 0 ]; do
+      declare -i order=$i+1
+      letter=`echo ${myhostname} | cut -c${order}`
 
-   # 文字に色を付ける
-   iro=${color[$i%6]}
-   if [ "${iro}" = "red" ]; then
-      letter="\[\033[31;1m\]${letter}\[\033[0m\]"
-   elif [ "${iro}" = yellow ]; then
-      letter="\[\033[33;1m\]${letter}\[\033[0m\]"
-   elif [ "${iro}" = green ]; then
-      letter="\[\033[32;1m\]${letter}\[\033[0m\]"
-   elif [ "${iro}" = cyan ]; then
-      letter="\[\033[36;1m\]${letter}\[\033[0m\]"
-   elif [ "${iro}" = blue ]; then
-      letter="\[\033[34;1m\]${letter}\[\033[0m\]"
-   elif [ "${iro}" = magenda ]; then
-      letter="\[\033[35;1m\]${letter}\[\033[0m\]"
-   fi
+      # Paint it rainbow
+      iro=${color[$i%6]}
+      if [ "${iro}" = "red" ]; then
+         letter="\[\033[31;1m\]${letter}\[\033[0m\]"
+      elif [ "${iro}" = yellow ]; then
+         letter="\[\033[33;1m\]${letter}\[\033[0m\]"
+      elif [ "${iro}" = green ]; then
+         letter="\[\033[32;1m\]${letter}\[\033[0m\]"
+      elif [ "${iro}" = cyan ]; then
+         letter="\[\033[36;1m\]${letter}\[\033[0m\]"
+      elif [ "${iro}" = blue ]; then
+         letter="\[\033[34;1m\]${letter}\[\033[0m\]"
+      elif [ "${iro}" = magenda ]; then
+         letter="\[\033[35;1m\]${letter}\[\033[0m\]"
+      fi
 
-   # 文字列を結合する
-   rainbowstr=${rainbowstr}${letter}
+      rainbowstr=${rainbowstr}${letter}
 
-   # 残りの文字数の計算など
-   let ++i
-   let --mojisu
-done
+      let ++i
+      let --mojisu
+   done
+   # Add back-slach to substitute them
+   rainbowstr=`echo ${rainbowstr}|sed -e "s:\\\\\\\\:\\\\\\\\\\\\\\\\:g"`
 
-export PS1="[\u@${rainbowstr}: \w]\$ "
+   # Substitute \h with raibowized \h
+   PS=`echo ${PS1} | sed -e "s:\\\\\h:${rainbowstr}:g"`
+
+   export PS1=${PS}
+else
+   echo ''
+   echo 'Your $PS1 does not have the hostname. Add "\h" to your $PS1.'
+   echo ''
+fi
 
